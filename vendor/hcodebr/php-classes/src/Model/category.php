01a58664f24b24cb8fe3806031a5adb5cd1db3fd
lucaskohori <lucas.kohori@gmail.com>
2018-12-03 17:26:50 -0200

    Paginacao

--- vendor/hcodebr/php-classes/src/Model/category.php
+++ vendor/hcodebr/php-classes/src/Model/category.php
@@ -5,6 +5,7 @@ namespace Hcode\Model;
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
+use \Hcode\Model\Product;
 
 class Category extends Model {
 	
@@ -103,6 +104,33 @@ class Category extends Model {
 
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 8) {
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products a 
+			INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct 
+			INNER JOIN tb_categories c ON c.idcategory = b.idcategory 
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+			", [
+				':idcategory'=>$this->getidcategory()
+			]);
+
+			$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+
+			return [
+				'data'=>Product::checkList($results),
+				'total'=>(int)$resultTotal[0]["nrtotal"],
+				'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+			];
+
+	}
+
 	public function addProduct(Product $product) {
 
 		$sql = new Sql();
@@ -111,7 +139,7 @@ class Category extends Model {
 			':idcategory'=>$this->getcategory(),
 			':idproduct'=>$product->getidproduct()
 		]);
-
+	
 	}
 
 	public function removeProduct(Product $product) {
