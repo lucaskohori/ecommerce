0aef75f86c55c12297bbfcf6bd29fbf5d4c07759
lucaskohori <lucas.kohori@gmail.com>
2018-12-04 13:45:09 -0200

    Visualização do produto

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">Android</a></li><li><a href="/categories/6">Apple</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/5">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">Android</a></li><li><a href="/categories/6">Apple</a></li><li><a href="/categories/7">Asus</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/5">Samsung</a></li>
