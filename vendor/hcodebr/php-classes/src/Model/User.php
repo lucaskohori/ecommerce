6d5175138baa114b32995931d026f4b296159c49
lucaskohori <lucas.kohori@gmail.com>
2019-01-03 16:26:10 -0200

    cadastro do usuário

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -288,6 +288,34 @@ public static function getForgot($email, $inadmin = true)
 
  }
 
+ public static function getErrorRegister() {
+
+ 	$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+ 	User::clearErrorRegister();
+
+ 	return $msg;
+
+ }
+
+ public static function clearErrorRegister() {
+
+ 	$_SESSION[User::ERROR_REGISTER] = NULL;
+
+ }
+
+ public static function checkLoginExist($login) {
+
+ 	$sql = new Sql();
+
+ 	$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+ 		':deslogin'=>$login
+ 	]);
+
+ 	return (count($results) > 0);
+
+ }
+
  public static function getPasswordHash($password) {
 
  	return password_hash($password, PASSWORD_DEFAULT, [
